3cb1211ec232065a923aa3d2c8eda80c10fb4394
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 13:10:43 +0200

    Add Vertex.getTraversibleEdges() test

--- src/Classes/Tests/Test_Vertex.py
+++ src/Classes/Tests/Test_Vertex.py
@@ -4,4 +4,11 @@ from src.Classes.Edge import Edge
 
 class TestVertex(unittest.TestCase):
 
-    pass
+    def test_getTraversibleEdges(self):
+        vertex1 = Vertex(1)
+        vertex2 = Vertex(2)
+        edge1 = Edge(vertex1, vertex2, 3)
+        edge2 = Edge(vertex1, vertex2, 4)
+        edge2.traversible = False
+
+        self.assertNotIn(edge2, vertex1.getTraversibleEdges())
