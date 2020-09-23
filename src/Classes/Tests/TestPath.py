9a96a11bd3a1259e8ba2e2ac4fbc331e6b663ec9
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 20:28:21 +0200

    Add TestPath file

---
+++ src/Classes/Tests/TestPath.py
@@ -0,0 +1,10 @@
+import unittest
+from src.Classes.Vertex import Vertex
+from src.Classes.Edge import Edge
+from src.Classes.Path import Path
+
+class TestPath(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
