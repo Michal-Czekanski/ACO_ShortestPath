ffc0fd7629f1e1688613e95bf5f67fde92f126d6
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 20:32:54 +0200

    Add test_constructor() in TestPath

--- src/Classes/Tests/TestPath.py
+++ src/Classes/Tests/TestPath.py
@@ -4,7 +4,21 @@ from src.Classes.Edge import Edge
 from src.Classes.Path import Path
 
 class TestPath(unittest.TestCase):
-    pass
+
+    def test_constructor(self):
+        vertex1 = Vertex(1)
+        vertex2 = Vertex(2)
+        vertex3 = Vertex(3)
+
+        edge1 = Edge(vertex1, vertex2, 1)
+        edge2 = Edge(vertex2, vertex3, 2)
+
+        path = Path(vertex1, [edge1, edge2], 1 + 2)
+
+        self.assertEqual(vertex1, path.beginning)
+        self.assertEqual(path.cost, 3)
+        self.assertIn(edge1, path.edges)
+        self.assertIn(edge2, path.edges)
 
 if __name__ == "__main__":
     unittest.main()
