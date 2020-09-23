4cc13e85819e831bb978f82845732e4cc4764f2c
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 14:27:55 +0200

    Add ctor in Path

--- src/Classes/Path.py
+++ src/Classes/Path.py
@@ -2,4 +2,7 @@ from src.Classes.Vertex import Vertex
 from src.Classes.Edge import Edge
 
 class Path:
-    pass
+    def __init__(self, beginning: Vertex, edges, cost: int):
+        self.beginning = beginning
+        self.edges = edges
+        self.cost = cost
