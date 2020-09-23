ae195875707fc9f2c97ec91eeef0171249570be4
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 13:50:09 +0200

    Add test_constructor() in TestEdge

--- src/Classes/Tests/Test_Edge.py
+++ src/Classes/Tests/Test_Edge.py
@@ -1,8 +1,15 @@
 import unittest
 from src.Classes.Edge import Edge
+from src.Classes.Vertex import Vertex
 
 class TestEdge(unittest.TestCase):
-    pass
+    def test_constructor(self):
+        vertex1 = Vertex(1)
+        vertex2 = Vertex(2)
+        edge = Edge(vertex1, vertex2, 3)
+
+        self.assertIn(edge, vertex1.edges)
+        self.assertIn(edge, vertex2.edges)
 
 if __name__ == "__main__":
     unittest.main()
