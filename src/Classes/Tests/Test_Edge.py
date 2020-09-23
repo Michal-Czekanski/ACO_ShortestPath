4d553697b6ea21f5697ddba0ca1c7ac6ac14ac1f
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-23 13:52:52 +0200

    Add test_getOtherEnd() in TestEdge

--- src/Classes/Tests/Test_Edge.py
+++ src/Classes/Tests/Test_Edge.py
@@ -11,5 +11,16 @@ class TestEdge(unittest.TestCase):
         self.assertIn(edge, vertex1.edges)
         self.assertIn(edge, vertex2.edges)
 
+    def test_getOtherEnd(self):
+        vertex1 = Vertex(1)
+        vertex2 = Vertex(2)
+        vertex3 = Vertex(3)
+
+        edge = Edge(vertex1, vertex2, 3)
+
+        self.assertEqual(edge.getOtherEnd(vertex1), vertex2)
+        self.assertEqual(edge.getOtherEnd(vertex2), vertex1)
+        self.assertEqual(edge.getOtherEnd(vertex3), None)
+
 if __name__ == "__main__":
     unittest.main()
