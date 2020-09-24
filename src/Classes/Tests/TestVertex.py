b6cfc90c0567bc0877515a1405b71bc8a369c324
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-24 10:37:59 +0200

    Add test_delete() in TestVertex cls

--- src/Classes/Tests/TestVertex.py
+++ src/Classes/Tests/TestVertex.py
@@ -13,5 +13,17 @@ class TestVertex(unittest.TestCase):
 
         self.assertNotIn(edge2, vertex1.getTraversibleEdges())
 
+    def test_delete(self):
+        vertex1 = Vertex(1)
+        vertex2 = Vertex(2)
+        edge1 = Edge(vertex1, vertex2, 3)
+        edge2 = Edge(vertex1, vertex2, 4)
+
+        vertex1.delete()
+
+        self.assertEqual(vertex1.ind, None)
+        self.assertNotIn(edge1, vertex2.edges)
+        self.assertNotIn(edge2, vertex2.edges)
+
 if __name__ == '__main__':
     unittest.main()
