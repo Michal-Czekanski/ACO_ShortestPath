07d84ca941d889f16c20600803ed502820b925f8
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-24 18:37:12 +0200

    Add constructor in Ant

--- src/Classes/Ant.py
+++ src/Classes/Ant.py
@@ -3,4 +3,7 @@ from src.Classes.Edge import Edge
 from src.Classes.Path import Path
 
 class Ant:
-    pass
+
+    def __init__(self, pheromoneInfluence: float, desirabilityInfluence: float):
+        self.pheromoneInfluence = pheromoneInfluence
+        self.desirabilityInfluence = desirabilityInfluence
