42629c2eb5d4bce8cd0967b001e5e9672ca54512
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-24 21:50:04 +0200

    Add TestAnt.py class file

---
+++ src/Classes/Tests/TestAnt.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.Classes.Ant import Ant
+from src.Classes.Vertex import Vertex
+from src.Classes.Edge import Edge
+from src.Classes.Path import Path
+
+class TestAnt:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
