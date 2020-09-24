f9ec7bc56a74ad799744391af6916065ef72ae5b
Michael-Czekanski <michal.cz2202@gmail.com>
2020-09-24 18:13:09 +0200

    Add TestGraph.py file with initial content

---
+++ src/Classes/Tests/TestGraph.py
@@ -0,0 +1,10 @@
+import unittest
+from src.Classes.Edge import Edge
+from src.Classes.Vertex import Vertex
+from src.Classes.Graph import Graph
+
+class TestGraph(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
