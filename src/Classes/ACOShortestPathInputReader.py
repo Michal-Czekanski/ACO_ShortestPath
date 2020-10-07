0bf06e41309d92ece543b0438bea5df998566244
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-07 13:31:37 +0200

    Add readGraphFromInputFile() static method

--- src/Classes/ACOShortestPathInputReader.py
+++ src/Classes/ACOShortestPathInputReader.py
@@ -1,2 +1,28 @@
+from src.Classes.Vertex import Vertex
+from src.Classes.Edge import Edge
+from src.Classes.Graph import Graph
+
 class ACOShortestPathInputReader:
-    pass
+    @staticmethod
+    def readGraphFromInputFile(inputFilename: str) -> Graph:
+        file = open(inputFilename)
+
+        vertexesNum = int(file.readline().strip())
+        edgesNum = int(file.readline().strip())
+
+        vertexes = [Vertex(i + 1) for i in range(vertexesNum)]
+
+        edges = []
+
+        line = file.readline().strip()
+        while line:
+            content = line.split()
+
+            vertexStart = int(content[1])
+            vertexEnd = int(content[2])
+            weight = int(content[3])
+
+            edges.append(Edge(vertexStart, vertexEnd, weight))
+            line = file.readline().strip()
+
+        return Graph(vertexes, edges)
