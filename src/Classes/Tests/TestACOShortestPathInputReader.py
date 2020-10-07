862962b6261ab8111188dff71861192b90937f4b
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-07 13:33:46 +0200

    Add TestACOShortestPathInputReader test class

---
+++ src/Classes/Tests/TestACOShortestPathInputReader.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.Classes.Vertex import Vertex
+from src.Classes.Edge import Edge
+from src.Classes.Graph import Graph
+
+class TestACOShortestPathInputReader(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
