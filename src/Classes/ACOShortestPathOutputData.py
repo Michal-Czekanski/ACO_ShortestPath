692599106f875be4a697c1667f43faf475a99c0f
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 20:47:23 +0200

    Add ACOShortestPathOutputData ctor

--- src/Classes/ACOShortestPathOutputData.py
+++ src/Classes/ACOShortestPathOutputData.py
@@ -1,2 +1,38 @@
+from src.Classes.Path import Path
+from datetime import datetime
+
+
 class ACOShortestPathOutputData:
-    pass
+    printAllPaths: bool
+    path: Path
+    elapsedTime: datetime
+    algorithmEndTime: datetime
+    algorithmStartTime: datetime
+    itersNum: int
+    evaporationCoefficent: float
+    desirabilityInfluence: float
+    pheromoneInfluence: float
+    vertexEndInd: int
+    vertexStartInd: int
+    runNumber: int
+    inputFilename: str
+
+    def __init__(self, runNumber: int, inputFilename: str, vertexStartInd: int, vertexEndInd: int,
+                 pheromoneInfluence: float,
+                 desirabilityInfluence: float,
+                 evaporationCoefficent: float, itersNum: int, algorithmStartTime: datetime, algorithmEndTime: datetime,
+                 elapsedTime: datetime, path: Path,
+                 printAllPaths: bool):
+        self.runNumber = runNumber
+        self.inputFilename = inputFilename
+        self.vertexStartInd = vertexStartInd
+        self.vertexEndInd = vertexEndInd
+        self.pheromoneInfluence = pheromoneInfluence
+        self.desirabilityInfluence = desirabilityInfluence
+        self.evaporationCoefficent = evaporationCoefficent
+        self.itersNum = itersNum
+        self.algorithmStartTime = algorithmStartTime
+        self.algorithmEndTime = algorithmEndTime
+        self.elapsedTime = elapsedTime
+        self.path = path
+        self.printAllPaths = printAllPaths
