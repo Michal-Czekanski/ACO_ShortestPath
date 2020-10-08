9127d08106c5d62e351717007c4d7688f3a26e41
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 22:53:53 +0200

    Add test_writeOutputToFile_withAllPaths(). Close #4

--- src/Classes/Tests/TestACOShortestPathOutputWriter.py
+++ src/Classes/Tests/TestACOShortestPathOutputWriter.py
@@ -10,6 +10,95 @@ import os
 
 
 class TestACOShortestPathOutputWriter(unittest.TestCase):
+    def test_writeOutputToFile_withAllPaths(self):
+        # Initialize data
+        testRunNumber = 1
+        testInputFilename = "testInputFilename.txt"
+        testVertexStartInd = 1
+        testVertexEndInd = 2
+        testPheromoneInfluence = 0.1
+        testDesirabilityInfluence = 0.1
+        testEvaporationCoefficent = 0.1
+        testItersNum = 10
+        testAlgorithmStartTime = datetime.datetime.strptime("2020_01_29_12_59", "%Y_%m_%d_%H_%M")
+        testAlgorithmEndTime = datetime.datetime.strptime("2020_01_29_13_59", "%Y_%m_%d_%H_%M")
+        testElapsedTime = testAlgorithmEndTime - testAlgorithmStartTime
+
+        v1 = Vertex(1)
+        v2 = Vertex(2)
+        e = Edge(v1, v2, 9)
+        testPath = Path(v1, [e], e.weight)
+
+        testPrintAllPaths = True
+        testPathToAllPathsFile = "testAllPathsFile.txt"
+        """
+        All paths file
+        """
+        allPathsFile = open(testPathToAllPathsFile, "w")
+        allPathsFileContent = """1. 1 - 2 --- weight = 9
+2. 1 - 2 --- weight = 9
+3. 1 - 2 --- weight = 9
+4. 1 - 2 --- weight = 9
+5. 1 - 2 --- weight = 9"""
+        allPathsFile.write(allPathsFileContent)
+        allPathsFile.close()
+        """
+        All paths file end
+        """
+
+        testData = ACOShortestPathOutputData(testRunNumber, testInputFilename, testVertexStartInd, testVertexEndInd,
+                                             testPheromoneInfluence, testDesirabilityInfluence,
+                                             testEvaporationCoefficent, testItersNum,
+                                             testAlgorithmStartTime, testAlgorithmEndTime, testElapsedTime,
+                                             testPath, testPrintAllPaths, testPathToAllPathsFile)
+
+        testOutputFilename = "test_writeOutputToFile_withAllPaths"
+        outputDataDirPath = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, "Data",
+                                                         "OutputResultFiles", "ResultsWithAllPathsPrinted"))
+        testOutputFilePath = os.path.join(outputDataDirPath, testOutputFilename)
+
+        # Testing
+        ACOShortestPathOutputWriter.writeOutputToFile(testOutputFilePath, testData)
+        os.remove(testPathToAllPathsFile)
+
+        expectedFileContent = """
+------------------------------------
+Run:          1
+Input:        testInputFilename.txt
+Parameters:
+    - vertex_start =            1
+    - vertex_end =              2
+    - pheromone_influence =     0.1
+    - desirability_influence =  0.1
+    - evaporation_coefficent =  0.1
+    - iters_num  =              10
+Start:        12:59:00 29/01/2020
+End:          13:59:00 29/01/2020
+Time running: 3600.0 sec
+
+Shortest path:
+weight =      9
+1 - 2 --- weight = 9
+------------------------------------
+All paths:
+1. 1 - 2 --- weight = 9
+2. 1 - 2 --- weight = 9
+3. 1 - 2 --- weight = 9
+4. 1 - 2 --- weight = 9
+5. 1 - 2 --- weight = 9"""
+
+        actualFileContent = ""
+
+        actualFile = open(testOutputFilePath, "r")
+        line = actualFile.readline()
+        while line:
+            actualFileContent += line
+            line = actualFile.readline()
+        actualFile.close()
+        os.remove(testOutputFilePath)
+
+        self.assertEqual(expectedFileContent, actualFileContent)
+
     def test_writeOutputToFile_withoutAllPaths(self):
 
         # Initialize data
@@ -77,10 +166,6 @@ weight =      9
         os.remove(testOutputFilePath)
 
         self.assertEqual(expectedFileContent, actualFileContent)
-
-
-
-
 
     def test_createOutputFilename(self):
         testLaunchingTimeAndDate = datetime.datetime.strptime("2020_01_29_12_59", "%Y_%m_%d_%H_%M")
