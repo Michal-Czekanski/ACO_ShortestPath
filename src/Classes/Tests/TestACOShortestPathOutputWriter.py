203514a7df46eeeeea973bb643e6f064d83a306c
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 18:47:34 +0200

    Add test_createOutputFilename()

--- src/Classes/Tests/TestACOShortestPathOutputWriter.py
+++ src/Classes/Tests/TestACOShortestPathOutputWriter.py
@@ -1,8 +1,31 @@
+from src.Classes.ACOShortestPathOutputWriter import ACOShortestPathOutputWriter
+
 import unittest
+import datetime
 
 
 class TestACOShortestPathOutputWriter(unittest.TestCase):
-    pass
+    def test_createOutputFilename(self):
+        testLaunchingTimeAndDate = datetime.datetime.strptime("2020_01_29_12_59", "%Y_%m_%d_%H_%M")
+        testInputFilename = "testInputFilename.txt"
+        testVertexStartInd = 1
+        testVertexEndInd = 2
+        testPheromoneInfluence = 0.1
+        testDesirabilityInfluence = 0.1
+        testEvaporationCoefficent = 0.1
+        testItersNum = 10
+
+        expectedOutputFilename = "testInputFilename_2020_01_29_12_59_1_2_0.1_0.1_0.1_10.txt"
+        resultOutputFilename = ACOShortestPathOutputWriter.createOutputFilename(testLaunchingTimeAndDate,
+                                                                                testInputFilename,
+                                                                                testVertexStartInd,
+                                                                                testVertexEndInd,
+                                                                                testPheromoneInfluence,
+                                                                                testDesirabilityInfluence,
+                                                                                testEvaporationCoefficent,
+                                                                                testItersNum)
+
+        self.assertEqual(expectedOutputFilename, resultOutputFilename)
 
 
 if __name__ == "__main__":
