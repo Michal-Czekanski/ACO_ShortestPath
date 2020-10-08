abd4f602b8f699da2499a308add1b84130081592
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 21:58:25 +0200

    Add empty TestACOShortestPathOutputData test class

---
+++ src/Classes/Tests/TestACOShortestPathOutputData.py
@@ -0,0 +1,11 @@
+from src.Classes.ACOShortestPathOutputData import ACOShortestPathOutputData
+
+import unittest
+
+
+class TestACOShortestPathOutputData(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
