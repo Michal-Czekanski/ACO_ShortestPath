2a2535e3954d974f953781dc9f041675cb08af80
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 21:53:04 +0200

    Add getDataAsString() in ACOShortestPathOutputData

--- src/Classes/ACOShortestPathOutputData.py
+++ src/Classes/ACOShortestPathOutputData.py
@@ -41,3 +41,42 @@ class ACOShortestPathOutputData:
         self.path = path
         self.printAllPaths = printAllPaths
         self.pathToAllPathsFile = pathToAllPathsFile
+
+    def getDataAsString(self) -> str:
+        """
+        :return: Data in neat format which can be written to file.
+        """
+        dataAsString = """
+------------------------------------
+Run:          {}
+Input:        {}
+Parameters:
+    - vertex_start =            {}
+    - vertex_end =              {}
+    - pheromone_influence =     {}
+    - desirability_influence =  {}
+    - evaporation_coefficent =  {}
+    - iters_num  =              {}
+Start:        {}
+End:          {}
+Time running: {}
+
+Shortest path:
+weight =      {}
+{}
+""".format(str(self.runNumber), self.inputFilename, str(self.vertexStartInd), str(self.vertexEndInd),
+           str(self.pheromoneInfluence), str(self.desirabilityInfluence), str(self.evaporationCoefficent),
+           str(self.itersNum),
+           self.algorithmStartTime.strftime(ACOShortestPathOutputData.timeFormatString),
+           self.algorithmEndTime.strftime(ACOShortestPathOutputData.timeFormatString),
+           str(self.elapsedTime.total_seconds()) + " sec",
+           str(self.path.cost),
+           self.path.pathAsString())
+
+        if self.printAllPaths:
+            # TODO:
+            pass
+
+        dataAsString += "------------------------------------"
+
+        return dataAsString
