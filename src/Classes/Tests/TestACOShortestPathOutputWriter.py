5bf83555accb155f17ba136f02389fb1fec7c2a8
Michael-Czekanski <michal.cz2202@gmail.com>
2020-10-08 20:37:38 +0200

    Add test_createOutputFile() test

--- src/Classes/Tests/TestACOShortestPathOutputWriter.py
+++ src/Classes/Tests/TestACOShortestPathOutputWriter.py
@@ -2,6 +2,7 @@ from src.Classes.ACOShortestPathOutputWriter import ACOShortestPathOutputWriter
 
 import unittest
 import datetime
+import os
 
 
 class TestACOShortestPathOutputWriter(unittest.TestCase):
@@ -26,6 +27,35 @@ class TestACOShortestPathOutputWriter(unittest.TestCase):
                                                                                 testItersNum)
 
         self.assertEqual(expectedOutputFilename, resultOutputFilename)
+
+    def test_createOutputFile(self):
+        testLaunchingTimeAndDate = datetime.datetime.strptime("2020_01_29_12_59", "%Y_%m_%d_%H_%M")
+        testInputFilename = "testInputFilename.txt"
+        testVertexStartInd = 1
+        testVertexEndInd = 2
+        testPheromoneInfluence = 0.1
+        testDesirabilityInfluence = 0.1
+        testEvaporationCoefficent = 0.1
+        testItersNum = 10
+
+        outputDataDirPath = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, "Data",
+                                                         "OutputResultFiles"))
+
+        expectedOutputFilePath = os.path.join(outputDataDirPath,
+                                              "testInputFilename_2020_01_29_12_59_1_2_0.1_0.1_0.1_10.txt")
+
+        resultOutputFilePath = ACOShortestPathOutputWriter.createOutputFile(outputDataDirPath, testLaunchingTimeAndDate,
+                                                                            testInputFilename,
+                                                                            testVertexStartInd,
+                                                                            testVertexEndInd,
+                                                                            testPheromoneInfluence,
+                                                                            testDesirabilityInfluence,
+                                                                            testEvaporationCoefficent,
+                                                                            testItersNum)
+
+        self.assertEqual(expectedOutputFilePath, resultOutputFilePath)
+        self.assertTrue(os.path.isfile(resultOutputFilePath))
+        os.remove(resultOutputFilePath)
 
 
 if __name__ == "__main__":
